8e73c5e1b11961e1be3800eb6d2c5273909cb2f8
fatou620 <diankofatoukine598@gmail.com>
2025-03-23 15:15:55 +0000

    Update Coureur.php

--- Coureur.php
+++ Coureur.php
@@ -73,7 +73,7 @@ FROM athelete a WHERE email ='$mAil' AND password='$mdP'";
 
  else{
      echo"email ou mot de passe incorrect ";
-  echo'<a href="index.html" retour > </a>'; 
+ // echo'<a href="index.html" retour > </a>'; 
  
   
 
